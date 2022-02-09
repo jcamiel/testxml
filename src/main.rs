70ba6d1169c1ccec19c09af8bb2232f0adffc23b
jcamiel <jeanchristophe.amiel@gmail.com>
2022-02-09 17:15:43 +0100

    Add xml sample.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
-use libxml::tree::Document;
+use libxml::tree::{Document, Node};
 
 fn main() {
-    let doc_result = Document::new();
-    assert!(doc_result.is_ok());
 }
+
+
+fn to_xml() -> String {
+    let doc = Document::new().unwrap();
+    let mut books = Node::new("books", None, &doc).unwrap();
+    books.add_text_child(None, "book", "Dune").unwrap();
+    books.add_text_child(None, "book", "Les Misérables").unwrap();
+    doc.node_to_string(&books)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::to_xml;
+
+    #[test]
+    fn it_works_0() {
+        assert_eq!(to_xml(), "<books><book>Dune</book><book>Les Misérables</book></books>");
+    }
+
+    #[test]
+    fn it_works_1() {
+        assert_eq!(to_xml(), "<books><book>Dune</book><book>Les Misérables</book></books>");
+    }
+
+    #[test]
+    fn it_works_2() {
+        assert_eq!(to_xml(), "<books><book>Dune</book><book>Les Misérables</book></books>");
+    }
+
+    #[test]
+    fn it_works_3() {
+        assert_eq!(to_xml(), "<books><book>Dune</book><book>Les Misérables</book></books>");
+    }
+
+}
+
+
+
